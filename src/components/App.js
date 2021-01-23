efc7e49865dbb3b8054a6ee52065c5dcce7322ed
raheela <raheelamoha@gmail.com>
2021-01-23 10:11:50 +0200

    create header

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import ProductList from './ProductList';
+import Header from './Header';
 
 const App = () => {
    
     return(
+        
         <div className="ui container">
-            
+        
+        
+                  <Header />
                   <ProductList header="Collections" subHeader="potatoes" productsGroup="products" />
                   <ProductList header="Shop" productsGroup="featuredProducts" />
                       
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,26 +1,28 @@
 import React from 'react';
+import {useMediaPredicate} from "react-media-hook";
 const imgStyle = {
-  width: "70%",
-  width: "65%"
-
+  width: "75%",
 }
+
 const ProductItem = ({price, name}) => {
+  const lessThan800 = useMediaPredicate("(max-width: 800px)");
     return(
-    <div className =" column">
+  <div className ={` column `}>
     <div className=" ui fluid card ">
-    <div style={{padding:"60px 10px", background:"grey",position:"relative"}} className="center aligned">
+    <div style={{padding:"46px 10px", background:"grey",position:"relative"}} className="center aligned">
   <a className="image" href="#">
     <img style={imgStyle} src="https://static.demilked.com/wp-content/uploads/2016/10/nikon-small-world-photo-contest-2016-16.jpg"/>
   </a>
   <p className="left floated" style={{position: "absolute", left:"0",bottom: "16px", background: "white",paddingRight:"16px"}}>&#36;{price}</p>
   </div>
-  <div className="content">
-     <div style={{marginBottom:"18px"}} className="center aligned">
+  { !lessThan800 &&
+    <div className="content">
+    <div style={{marginBottom:"18px"}} className="center aligned">
     <a className="header" href="#">{name}</a><br></br>
     <span >color</span>
     </div>
     <div className="meta">
-    <div className="tiny ui left labeled button" tabindex="0">
+    <div className="tiny ui left labeled button" tabIndex="0">
   <a className="ui basic label">
     Add to cart
   </a>
@@ -32,6 +34,10 @@ const ProductItem = ({price, name}) => {
       <a className={`tiny ui primary button ${window.innerWidth>1200?"right floated":" "} `}>Buy now</a>
     </div>
   </div>
+
+  }
+  
+  
 </div>
 </div>
 
