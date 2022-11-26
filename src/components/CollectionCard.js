db92a90c4c47762bf41acc5f4c4c31adf40a2231
rahila Ilegbodu <raheelamoha@gmail.com>
2022-11-26 21:04:30 +0100

    remove comments from collection card

--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -7,18 +7,12 @@ import styled from 'styled-components';
 const CollectionCard = styled.div`
   margin-bottom: 1rem;
   border-radius: 8px;
-  // overflow: hidden;
   border: 1px solid lightgrey;
-  // box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
   cursor: pointer;
-  // padding: 4px;
-
   &:hover {
     background: rgba(247, 235, 212, 0.1);
-    // padding: 0;
-    // border: 4px solid #f7ebd4;
   }
 `;
 
