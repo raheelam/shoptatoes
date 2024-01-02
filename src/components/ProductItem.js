87f7ef75cbc52b04e70b83f43a54935189b46748
raheelam <39638440+raheelam@users.noreply.github.com>
2024-01-02 23:32:01 +0300

    Update ProductItem.js
    Remove commented overflow

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -7,7 +7,6 @@ import styled from 'styled-components';
 const ProductCard = styled.div`
   margin-bottom: 1rem;
   border-radius: 8px;
-  // overflow: hidden;
   border: 1px solid lightgrey;
   // box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
